9a95a35f9acd446adebeb041c4c3f4a4d78195f1
Gabriel Reis <gabrielreis2701@gmail.com>
2021-12-01 17:28:24 -0400

    Jogo Pronto

--- script.js
+++ script.js
@@ -51,6 +51,7 @@ function update (event){
 }
 
 function iniciarJogo(){
+    
     if(snake[0].x > 15 * box && direction == "right")
         snake[0].x = 0;
     if(snake[0].x < 0 && direction =="left") 
@@ -59,6 +60,14 @@ function iniciarJogo(){
         snake[0].y = 0;
     if(snake[0].y < 0 && direction =="up") 
         snake[0].y = 16 * box;
+
+    
+    for(i=1; i < snake.length; i++){
+        if(snake[0].x == snake[i].x && snake[0].y == snake[i].y){
+            clearInterval(jogo);
+            alert('Gamer Over (-_-)' );
+        }
+    }
 
     criarBG();
     criarCobrinha();
